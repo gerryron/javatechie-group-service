b2e84189a162fa49a7876d790bc8e9cf75de36c4
gerryron <gerryron02@gmail.com>
2022-10-05 03:52:50 +0700

    initial app

---
+++ src/main/java/com/yhoboo/group/JavatechieGroupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yhoboo.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatechieGroupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatechieGroupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhoboo/group/JavatechieGroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhoboo.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatechieGroupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
